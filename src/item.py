8628f74399c73c8a66a60427fe94718a74521284
Nastya Foxe <Kukusiki2204@gmail.com>
2024-01-22 21:42:35 +0300

    добавила функционала в класс Item

--- src/item.py
+++ src/item.py
@@ -1,3 +1,7 @@
+import csv
+from pathlib import Path
+
+
 class Item:
     """Класс. реализующий представления товара в магазине."""
     pay_rate = 1.0
@@ -5,7 +9,7 @@ class Item:
 
     def __init__(self, name: str, price: float, quantity: int) -> None:
         """Метод, который инициализирует экземпляры класса."""
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
         self.all.append(self)
@@ -17,3 +21,35 @@ class Item:
     def apply_discount(self) -> None:
         """Применяет установленную скидку для конкретного товара."""
         self.price *= Item.pay_rate
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, name):
+        if len(name) > 10:
+            name = name[:10]
+        self.__name = name
+
+    @classmethod
+    def instantiate_from_csv(cls, file_path: str) -> None:
+        """
+        Инициализирует экземпляры класса Item данными из CSV - файла.
+        """
+        current_file_path = Path(__file__)
+        file_path = current_file_path.parent.parent / 'src/items.csv'
+
+        with open(file_path, 'r', encoding='utf-8') as file:
+            cls.all.clear()
+            reader = csv.DictReader(file)
+            for row in reader:
+                name = row['name']
+                price = float(row['price'])
+                quantity = int(row['quantity'])
+                cls(name, price, quantity)
+
+    @staticmethod
+    def string_to_number(string):
+        number = round(float(string))
+        return number
